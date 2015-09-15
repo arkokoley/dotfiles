c5c9d53c78cb4087e29bfc02c9cb4b5a5e78b17b
Gaurav Koley <arkokoley@live.in>
2015-09-16 01:52:51 +0530

    Move to Antigen from OMZ for ZSH

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 sudo apt-get install python-pip git zsh vim vim-gnome cmake python-dev python3-dev
 
-git clone https://github.com/Lokaltog/powerline-fonts --depth=1
-cd powerline-fonts
-sh install.sh
-cd .. && rm powerline-fonts -r
+#git clone https://github.com/Lokaltog/powerline-fonts --depth=1
+#cd powerline-fonts
+#sh install.sh
+#cd .. && rm powerline-fonts -r
 
-pip install --user git+git://github.com/Lokaltog/powerline
+#pip install --user git+git://github.com/Lokaltog/powerline
 
 cd vim/bundle/
 rm Vundle.vim -r
