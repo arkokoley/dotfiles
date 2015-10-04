9c1f2fb5996afc9d4fd6beb8c221be7cd3448594
Gaurav Koley <arkokoley@live.in>
2015-10-04 16:44:33 +0530

    Update bootstrap.sh and add git commit template
    YCM was removed, so the compiling instructions for YCM have been
    removed. Also, VimShell and vimproc were installed which require
    a compilation step.
    Additionally a template git commit message was added.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,26 @@
 sudo apt-get install python-pip git zsh vim vim-gnome cmake python-dev python3-dev exuberant-ctags
 
-#git clone https://github.com/Lokaltog/powerline-fonts --depth=1
-#cd powerline-fonts
-#sh install.sh
-#cd .. && rm powerline-fonts -r
-
-#pip install --user git+git://github.com/Lokaltog/powerline
+git clone https://github.com/Lokaltog/powerline-fonts --depth=1
+cd powerline-fonts
+sh install.sh
+cd .. && rm powerline-fonts -r
 
 cd vim/bundle/
 rm Vundle.vim -r
 git clone https://github.com/gmarik/Vundle.vim
+cd ../..
 
 [[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME/.vimrc-old
-[[ -f $HOME/.zshrc ]] && mv $HOME/.zshrc $HOME/.zshrc-old
-
-cd ../..
 ln -s $PWD/vim/vimrc $HOME/.vimrc
+
+[[ -f $HOME/.zshrc ]] && mv $HOME/.zshrc $HOME/.zshrc-old
 ln -s $PWD/zsh/zshrc $HOME/.zshrc
+
 chsh -s /bin/zsh
 vim +PluginInstall
-cd $HOME/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-./install.py
-cd ../../..
+
+cd $HOME/.vim/bundle/vimproc.vim/
+make
+cd
+
+echo "Done!"
