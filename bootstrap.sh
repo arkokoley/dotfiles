826467aff575f3270a92fb5aeb43d1a89e8be216
Gaurav Koley <arkokoley@live.in>
2018-01-12 20:41:47 +0530

    z and fzf with ag

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,24 @@
-sudo apt-get install python-pip git zsh vim vim-gnome cmake python-dev python3-dev exuberant-ctags
+#sudo apt-get install python-pip git zsh vim vim-gnome cmake python-dev python3-dev exuberant-ctags silversearcher-ag
 
-git clone https://github.com/Lokaltog/powerline-fonts --depth=1
-cd powerline-fonts
-sh install.sh
-cd .. && rm powerline-fonts -r
+#git clone https://github.com/Lokaltog/powerline-fonts --depth=1
+#cd powerline-fonts
+#sh install.sh
+#cd .. && rm powerline-fonts -r
 
-[[ -f $HOME/.zshrc ]] && mv $HOME/.zshrc $HOME/.zshrc-old
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O $HOME/.files/zsh/z
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.files/fzf
+
 mkdir $HOME/.files/
-mkdir $HOME/.files/zsh
-cp zsh/* $HOME/.files/zsh/. -r
-ln -s $HOME/.files/zsh/zshrc $HOME/.zshrc
+cp * $HOME/.files/. -r
+cp .git $HOME/.files/. -r
+echo "source $HOME/.files/zsh/zshrc" >> $HOME/.zshrc
 
 chsh -s /bin/zsh
+$HOME/.files/fzf/install
 
 read -p "Install Arcana-Vim? [y/n]" answer
 if [ $answer = y ] ; then
-  git clone https://github.com/arkokoley/arcana-vim $HOME/.files/arcana-vim
+  git clone --depth 1 https://github.com/arkokoley/arcana-vim $HOME/.files/arcana-vim
   cd $HOME/.files/arcana-vim
   ./bootstrap.sh
   rm -f -r $HOME/.files/arcana-vim/
